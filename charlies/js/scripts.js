a229a22d48dfcd8385e261a5d7d2a97395e1522b
MyaMya2001 <97988355+MyaMya2001@users.noreply.github.com>
2022-03-01 20:58:15 -0700

    March 01 changes

--- charlies/js/scripts.js
+++ charlies/js/scripts.js
@@ -1,3 +1,15 @@
 $(".my-button").click(function () {
     $("nav").toggleClass("open");
   });
+
+  $(document).ready(function(){
+    /*
+We're finding the element and hiding it here
+    */
+    $('nav').hide();
+
+    // Setup our click function
+    $('.menu-button').click(function(){
+        $('nav').slideToggle();
+    });
+});
